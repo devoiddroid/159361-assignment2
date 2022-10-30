90cbd4e4e7e63be5b71f3e11280d702544f6d10b
Jamie R <55900188+jr1000000@users.noreply.github.com>
2022-10-30 19:44:35 +1300

    Bear attack audio

---
+++ Assignment 2/Assets/Scripts/BearAudio.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearAudio : MonoBehaviour
+{
+    public AudioClip audioClip;
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Roar on attack
+    private void Roar(){
+        audioSource.PlayOneShot(audioClip);
+    }
+}
